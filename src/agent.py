1f7de7bca821aa948367b451e2bdb8ec3dac106f
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-06-07 20:18:22 -0500

    Update setup

--- src/agent.py
+++ src/agent.py
@@ -1,6 +1,106 @@
 import numpy as np
 import gym
 import random
-import time
 
-print("Hello")
+# Environment
+env = gym.make("FrozenLake-v1")
+
+# Dimensions of the state and action table
+action_space_size = env.action_space.n
+state_space_size = env.observation_space.n
+
+# Define the Q-table
+q_table = np.zeros((state_space_size, action_space_size))
+print(f"Q-Table - \n{q_table}\nShape: {q_table.shape}")
+
+
+# Hyperparameters
+total_episodes = 15000        # Total episodes
+learning_rate = 0.8           # Learning rate
+max_steps = 99                # Max steps per episode
+gamma = 0.95                  # Discounting rate
+
+# Exploration parameters
+epsilon = 1.0                 # Exploration rate
+max_epsilon = 1.0             # Exploration probability at start
+min_epsilon = 0.01            # Minimum exploration probability 
+decay_rate = 0.005             # Exponential decay rate for exploration prob
+
+
+# List of rewards
+rewards = []
+
+# Q-Learning algorithm
+
+# For life or until learning is stopped
+for episode in range(total_episodes):
+    # Reset the environment
+    state = env.reset()
+    step = 0
+    done = False
+    total_rewards = 0
+    
+    for step in range(max_steps):
+        # 3. Choose an action a in the current world state (s)
+        ## First we randomize a number
+        exp_exp_tradeoff = random.uniform(0, 1)
+        
+        ## If this number > greater than epsilon --> exploitation (taking the biggest Q value for this state)
+        if exp_exp_tradeoff > epsilon:
+            action = np.argmax(q_table[state,:])
+
+        # Else doing a random choice --> exploration
+        else:
+            action = env.action_space.sample()
+
+        # Take the action (a) and observe the outcome state(s') and reward (r)
+        new_state, reward, done, info = env.step(action)
+
+        # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+        # qtable[new_state,:] : all the actions we can take from new state
+        q_table[state, action] = q_table[state, action] + learning_rate * (reward + gamma * np.max(q_table[new_state, :]) - q_table[state, action])
+        
+        total_rewards += reward
+        
+        # Our new state is state
+        state = new_state
+        
+        # If done (if we're dead) : finish episode
+        if done == True: 
+            break
+        
+    # Reduce epsilon (because we need less and less exploration)
+    epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode) 
+    rewards.append(total_rewards)
+
+print ("Score over time: " +  str(sum(rewards)/total_episodes))
+print(q_table)
+
+
+# PLaying the game
+
+env.reset()
+
+for episode in range(5):
+    state = env.reset()
+    step = 0
+    done = False
+    print("****************************************************")
+    print("EPISODE ", episode)
+
+    for step in range(max_steps):
+        
+        # Take the action (index) that have the maximum expected future reward given that state
+        action = np.argmax(q_table[state,:])
+        
+        new_state, reward, done, info = env.step(action)
+        
+        if done:
+            # Here, we decide to only print the last state (to see if our agent is on the goal or fall into an hole)
+            env.render()
+            
+            # We print the number of step it took.
+            print("Number of steps", step)
+            break
+        state = new_state
+env.close()
